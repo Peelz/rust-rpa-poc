9aa5a26dda55912cb9bb66c465e42e67881624dd
Anucha Mai <anucha.mai@truedigital.com>
2025-08-05 01:38:07 +0700

    update utils parse buddist

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,3 +1,2 @@
 pub mod protocol;
-pub mod gcp;
 pub mod utils;
--- common/src/utils.rs
+++ common/src/utils.rs
@@ -4,20 +4,17 @@ pub fn parse_buddhist_date(
     date_str: &str,
 ) -> Result<OffsetDateTime, Box<dyn std::error::Error>> {
     // Parse the input date string: "01/01/2568"
-    let parts: Vec<u8> = date_str
-        .split('/')
-        .map(|s| s.parse::<u8>())
-        .collect::<Result<_, _>>()?;
+    let parts: Vec<&str> = date_str.split('/').collect();
 
     if parts.len() != 3 {
         return Err("Invalid date format".into());
     }
 
-    let day = parts[0];
-    let month = parts[1];
-    let year_be = parts[2];
+    let day: u8 = parts[0].parse()?;
+    let month: u8 = parts[1].parse()?;
+    let year_be: i32 = parts[2].parse()?;
 
-    let year_ad = (year_be as i32) - 543;
+    let year_ad = year_be - 543;
     let month_enum = Month::try_from(month)?;
 
     let date = Date::from_calendar_date(year_ad, month_enum, day)?;
@@ -28,4 +25,49 @@ pub fn parse_buddhist_date(
     let offset_dt = primitive_dt.assume_offset(UtcOffset::UTC);
 
     Ok(offset_dt)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use time::macros::datetime;
+
+    #[test]
+    fn test_parse_buddhist_date_success_1() {
+        let date_str = "01/01/2568";
+        let expected_dt = datetime!(2025-01-01 00:00:00 UTC);
+        let result = parse_buddhist_date(date_str).unwrap();
+        assert_eq!(result, expected_dt);
+    }
+
+
+        #[test]
+    fn test_parse_buddhist_date_success_2() {
+        let date_str = "31/12/2568";
+        let expected_dt = datetime!(2025-12-31 00:00:00 UTC);
+        let result = parse_buddhist_date(date_str).unwrap();
+        assert_eq!(result, expected_dt);
+    }
+
+
+    #[test]
+    fn test_parse_buddhist_date_invalid_format() {
+        let date_str = "01-01-2568";
+        let result = parse_buddhist_date(date_str);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_parse_buddhist_date_invalid_month() {
+        let date_str = "01/13/2568";
+        let result = parse_buddhist_date(date_str);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_parse_buddhist_date_invalid_day() {
+        let date_str = "32/01/2568";
+        let result = parse_buddhist_date(date_str);
+        assert!(result.is_err());
+    }
 }
